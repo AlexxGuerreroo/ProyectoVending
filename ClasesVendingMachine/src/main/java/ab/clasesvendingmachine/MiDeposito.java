038054c510d6237b41ed0b86b3f90b48c8408634
Xelemental <xelemental864@gmail.com>
2022-01-18 14:41:51 +0100

    Para probar las respectivas clases

---
+++ ClasesVendingMachine/src/main/java/ab/clasesvendingmachine/MiDeposito.java
@@ -0,0 +1,30 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package ab.clasesvendingmachine;
+
+/**
+ *
+ * @author fco-j
+ */
+public class MiDeposito {
+
+    /**
+     * @param args the command line arguments
+     */
+    
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        Deposito pipo = new Deposito(15,15,15,15,15,15,15,15);
+        Deposito fuckingDead = new Deposito();
+        Deposito doge = new Deposito(15,15,15,15,15,15,15,15,1234.56);
+        
+        System.out.println(pipo);
+        System.out.println(fuckingDead);
+        System.out.println(doge);
+        
+    }
+    
+}
---
+++ ClasesVendingMachine/src/main/java/ab/clasesvendingmachine/MiFactura.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package ab.clasesvendingmachine;
+
+/**
+ *
+ * @author fco-j
+ */
+public class MiFactura {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        Articulo noRandom = new Articulo("Kit Kat", 1.60, 10, 236);
+        
+        
+        
+    }
+    
+}
