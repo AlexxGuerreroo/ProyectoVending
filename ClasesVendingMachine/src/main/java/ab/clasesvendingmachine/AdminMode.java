ceb7f5d831705eebca92de09d2c12eec4086637b
Xelemental <xelemental864@gmail.com>
2022-01-26 21:47:09 +0100

    POR TERMINAR Establecidos los métodos de consultarBandejas, consultarDeposito, recaudarDelDeposito y recargarDeposito

---
+++ ClasesVendingMachine/src/main/java/ab/clasesvendingmachine/AdminMode.java
@@ -0,0 +1,70 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ab.clasesvendingmachine;
+
+import java.time.LocalDate;
+
+/**
+ *
+ * @author pikac
+ */
+public class AdminMode {
+    
+    private final Maquina maquina;
+
+    public AdminMode(Maquina maquina) {
+        
+        this.maquina = maquina;
+        
+        optionSelect();
+        
+    }
+    
+    public void optionSelect(){
+        //Este método indicará los métodos para poder seleccionarse:
+        
+        
+        
+    }
+            
+    public void consultarBandejas(){
+        
+        System.out.println(maquina.getA1());
+        System.out.println(maquina.getA2());
+        System.out.println(maquina.getA3());
+        System.out.println(maquina.getA4());
+        System.out.println(maquina.getA5());
+        System.out.println(maquina.getA6());
+        System.out.println(maquina.getA7());
+        System.out.println(maquina.getA8());
+        
+    }
+    
+    public void consultarDeposito(){
+        
+        System.out.println(maquina.getDeposito());
+        
+    }
+    
+    public void recaudarDelDeposito(){
+        
+        maquina.getDeposito().vaciarDeposito();
+        
+        maquina.getDeposito().setFechaUltRecaudacion(LocalDate.now());
+        
+    }
+    
+    public void recargarDeposito(int m10c, int m20c, int m50c, int m1e, int m2e, 
+            int b5e, int b10e, int b20e){
+        
+        maquina.getDeposito().recargar(m10c, m20c, m50c, m1e, m2e, b5e, b10e
+                , b20e);
+        
+        maquina.getDeposito().setFechaUltRecarga(LocalDate.now());
+        
+    }
+    
+}
