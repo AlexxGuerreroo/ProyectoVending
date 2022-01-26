0dbebdf82c91b74f6b61cd7d662fa840b0e025fe
Alex Lopez Guerrero <alexx_guerreroo@outlook.es>
2022-01-26 21:05:27 +0100

    Comentarios actualizados

--- ClasesVendingMachine/src/main/java/ab/clasesvendingmachine/Tarjeta.java
+++ ClasesVendingMachine/src/main/java/ab/clasesvendingmachine/Tarjeta.java
@@ -16,14 +16,14 @@ import java.util.*;
 
 public class Tarjeta {
 
-    //Declaramos las variables dentro de la clase 
+    //Declaramos las variables dentro de la clase .
     private String numTarjeta;
     private String nombre;
     private String apellido1;
     private String apellido2;
     private String banco;
     private String cvv;
-    private double saldo;//Con cada pago, se reducirá esta cantidad
+    private double saldo;//Con cada pago, se reducirá esta cantidad.
     private boolean valido;
     private LocalDate caducidad;
     private static int contador = 0;
@@ -31,59 +31,61 @@ public class Tarjeta {
     //Por último, estas dos clases determinan la cantidad mínima y máxima que 
     //puede haber de saldo:
     public static final double MIN = 1.50;//Mínima cantidad de dinero al crear
-    //la tarjeta
+    //la tarjeta.
     public static final double MAX = 3000;//Máxima cantidad de dinero al crear
-    //la tarjeta
+    //la tarjeta.
     
     public Tarjeta(String numTarjeta, String nombre, String apellido1,
             String apellido2, String banco, String cvv, double saldo, int mes,
             int ano) {//Constructor para el método copiar:
 
-        //Comprobamos que el número de la tarjeta y el nº secreto son válidos               
-        if (validezNum(numTarjeta)){//En caso afirmativo, ponemos los datos
+        //Comprobamos que el número de la tarjeta y el nº secreto son válidos 
+        //En caso afirmativo, ponemos los datos
+        if (validezNum(numTarjeta)){
             
             this.numTarjeta = numTarjeta;
             
-        }else{//Si no, se pondrán datos por defecto
+        }else{//Si no, se pondrán datos por defecto.
             
             System.out.println("Nº de tarjeta introducido no es válido");
             this.numTarjeta = "0123456789012345";
             
         }
         
-        if (validezCVV(cvv)) {//En caso afirmativo, ponemos los datos
+        //En caso afirmativo, ponemos los datos
+        if (validezCVV(cvv)) {
             
             this.cvv = cvv;
             
-        } else {//Si no, se pondrán datos por defecto
+        } else {    //Si no, se pondrán datos por defecto
             
             System.out.println("Nº secreto introducido no es válido");
             this.cvv = "012";
         }
 
-        //Pasamos el nombre y apellidos
+        //Pasamos el nombre y los apellidos.
         this.nombre = nombre;
         this.apellido1 = apellido1;
         this.apellido2 = apellido2;
 
-        //Pasamos el nombre de la entidad bancaria
+        //Pasamos el nombre de la entidad bancaria.
         this.banco = banco;
 
         //Se pasa el valor parametrizado del saldo:
-
-            this.saldo = saldo;
+        this.saldo = saldo;
 
         
 
-        //Pasamos la fecha de caducidad y comprobamos si la fecha está cumplida
+        //Pasamos la fecha de caducidad y comprobamos si la fecha está cumplida.
         this.caducidad = LocalDate.of(ano, mes, 1);
         if (this.caducado()) {
-            //Si ya ha pasado la fecha de caducidad, se reestablece por defecto:
+            
             this.caducidad = LocalDate.of(2077, 9, 13);
+         //Si ya ha pasado la fecha de caducidad, se reestablece por defecto:
         }
 
-        //Se crea la tarjeta siempre sin ser validada, pudiéndose 
-        //activar con un método aparte
+        //Se crea una tarjeta por defecto que no será validada, pero pudiéndose 
+        //activar posteriormente con un método.
         valido = false;
 
     }
@@ -92,7 +94,7 @@ public class Tarjeta {
     
     public Tarjeta(String numTarjeta, String cvv, int mes, int ano) {
         //Constructor parametrizado:
-        Random tombola = new Random();
+        Random ruleta = new Random();
 
         if(this.validezNum(numTarjeta)){
             
@@ -115,19 +117,19 @@ public class Tarjeta {
         }
         
         this.caducidad = LocalDate.of(ano, mes, 1);
-        nombre = "User";//Valor por defecto
-        apellido1 = "";//Valor por defecto
-        apellido2 = "";//Valor por defecto
-        banco = "PrepH";//Valor por defecto
-        saldo = tombola.nextInt(3000) + 1;//Se genera el saldo al azar
-        valido = false;//Se establece como anulada.
-
-        //Aumentamos en 1 el contador:
-        contador++;
+        
+        //Valores creados por defecto.
+        nombre = "User";
+        apellido1 = "";
+        apellido2 = "";
+        banco = "PrepH";
+        saldo = ruleta.nextInt(3000) + 1;//El saldo es generado al azar.
+        valido = false;//Por defecto se crea anulada.
+        contador++; //Aumentamos en 1 el contador.
 
     }
 
-    public Tarjeta() {//Constructor por defecto
+    public Tarjeta() {//Constructor por defecto.
         //Introducimos valores por defecto:
         numTarjeta = "0123456789012345";
         cvv = "012";
@@ -137,10 +139,8 @@ public class Tarjeta {
         banco = "Firewatch";
         saldo = 803.19;
         caducidad = LocalDate.of(2077, 9, 13);
-        valido = false; //Siempre empieza anulada
-
-        //Aumentamos en 1 el contador:
-        contador++;
+        valido = false; //Por defecto se crea anulada.
+        contador++; //Aumentamos en 1 el contador.
 
         /*PD: Los valores del nombre, apellidos y la fecha son relacionados a un
         meme (este: https://www.youtube.com/watch?v=huie2_3Pekg ) */
@@ -151,15 +151,15 @@ public class Tarjeta {
         boolean v = true;
         char d;
 
-        //Comprueba si el número de tarjeta (nT) y el CVV tienen la longitud 
-        //exacta:
+        //Comprueba si el número de tarjeta (nT) tienen la longitud exacta:
         if (nT.length() == 16) {
 
             for (int i = 0; i < 16; i++) {//Comprueba que cada parte del string
-                //es un nº
+                //es un número.
 
                 d = nT.charAt(i);
                 if (!(Character.isDigit(d))) {
+                    
                     //Si hay un caracter no numérico, salta el mensaje de error
                     System.out.println("El nº de tarjeta es inválido "
                             + "(no es numérico)");
@@ -169,8 +169,7 @@ public class Tarjeta {
 
             }            
 
-        } else {//Si el número de tarjeta no es de 16 
-            //cifras...
+        } else {//Si el número de tarjeta no es de 16 cifras salta el error.
 
             System.out.println("El nº de tarjeta es inválido (fallo de longitud");
             v = false;
@@ -186,15 +185,15 @@ public class Tarjeta {
         boolean v = true;
         char d;
 
-        //Comprueba si el número de tarjeta (nT) y el CVV tienen la longitud 
-        //exacta:
+        //Comprueba CVV tienen la longitud exacta:
         if (cvv.length() == 3) {
 
             for (int j = 0; j < 3; j++) {//Comprueba que cada parte del string
-                //es un nº
+                //es un número.
 
                 d = cvv.charAt(j);
                 if (!(Character.isDigit(d))) {
+                    
                     //Si hay un caracter no numérico, salta el mensaje de error
                     System.out.println("El CVV es inválido (no es numérico)");
                     v = false;
@@ -203,7 +202,7 @@ public class Tarjeta {
 
             }
 
-        } else {//si el CVV no es de 3 cifras...
+        } else {//si el CVV no es de 3 cifras salta el error.
 
             System.out.println("El CVV es inválido (fallo de longitud)");
             v = false;
@@ -213,20 +212,23 @@ public class Tarjeta {
         return v;
 
     }
-
-    public boolean caducado() {//Comprueba si la tarjeta ha caducado
+    
+    //Método para comprobar la validez de la tarjeta.
+    public boolean caducado() {
 
         LocalDate ahora = LocalDate.now();
         boolean caduco;
-        if (!(ahora.isBefore(caducidad))) {//Si el día actual no es antes de
-            //la fecha de caducidad, se indica que la tarjeta ha caducado:
+        if (!(ahora.isBefore(caducidad))) {
+            //Si el día actual no es antior a la fecha de caducidad, se indica
+            //que la tarjeta ha caducado.
 
             caduco = true;
             System.out.println("Esta tarjeta ya está caducada");
             this.valido = false;
 
-        } else {//Si el día actual es antes de la fecha de caducidad:
-
+        } else {
+            //Si el día actual es anterior a la fecha de caducidad, devolverá 
+            //un valor falso.
             caduco = false;
 
         }
@@ -334,7 +336,9 @@ public class Tarjeta {
         this.valido = false;
 
     }
-
+    
+    //Método para efectuar el pago en el caso de que fuesen válidos todos los 
+    //valores, o en su defecto devolver un error.
     public boolean pago(double coste) {
         Scanner entry = new Scanner(System.in);
         String cvv;
@@ -365,7 +369,8 @@ public class Tarjeta {
         return pago;
 
     }
-
+    
+    //Método copiar.
     public static Tarjeta copiar(Tarjeta c) {
 
         Tarjeta aux = new Tarjeta(c.getNumTarjeta(), c.getNombre(), 
