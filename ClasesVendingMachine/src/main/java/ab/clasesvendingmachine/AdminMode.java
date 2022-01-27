0d11907f5bb1bd927d33e097e6e5c2b95ba7458b
Xelemental <xelemental864@gmail.com>
2022-01-27 11:00:16 +0100

    POR TERMINAR (y por comentar) Implementados los métodos cambiarCodigo, cambiarProducto y cambiarStock

--- ClasesVendingMachine/src/main/java/ab/clasesvendingmachine/AdminMode.java
+++ ClasesVendingMachine/src/main/java/ab/clasesvendingmachine/AdminMode.java
@@ -12,27 +12,25 @@ import java.time.LocalDate;
  * @author pikac
  */
 public class AdminMode {
-    
+
     private final Maquina maquina;
 
     public AdminMode(Maquina maquina) {
-        
+
         this.maquina = maquina;
-        
+
         optionSelect();
-        
+
     }
-    
-    public void optionSelect(){
+
+    public void optionSelect() {
         //Este método indicará los métodos para poder seleccionarse:
-        
-        
-        
+
     }
-            
-    public void consultarBandejas(){
+
+    public void consultarBandejas() {
         //Muestra todos los artículos (bandejas) de la máquina:
-        
+
         System.out.println(maquina.getA1());
         System.out.println(maquina.getA2());
         System.out.println(maquina.getA3());
@@ -41,36 +39,165 @@ public class AdminMode {
         System.out.println(maquina.getA6());
         System.out.println(maquina.getA7());
         System.out.println(maquina.getA8());
-        
+
     }
-    
-    public void consultarDeposito(){
+
+    public void consultarDeposito() {
         //Muestra los datos del depósito de la máquina:
-        
+
         System.out.println(maquina.getDeposito());
-        
+
     }
-    
-    public void recaudarDelDeposito(){
+
+    public void recaudarDelDeposito() {
         //Vacía el depósito para recaudar beneficios y establece la fecha de la 
         //acción acordemente:
-        
+
         maquina.getDeposito().vaciarDeposito();
-        
+
         maquina.getDeposito().setFechaUltRecaudacion(LocalDate.now());
-        
+
     }
-    
-    public void recargarDeposito(int m10c, int m20c, int m50c, int m1e, int m2e, 
-            int b5e, int b10e, int b20e){
+
+    public void recargarDeposito(int m10c, int m20c, int m50c, int m1e, int m2e,
+            int b5e, int b10e, int b20e) {
         //Recarga el depósito para tener cambio y establece la fecha de la acción
         //acordemente:
-        
-        maquina.getDeposito().recargar(m10c, m20c, m50c, m1e, m2e, b5e, b10e
-                , b20e);
-        
+
+        maquina.getDeposito().recargar(m10c, m20c, m50c, m1e, m2e, b5e, b10e,
+                 b20e);
+
         maquina.getDeposito().setFechaUltRecarga(LocalDate.now());
-        
+
     }
-    
+
+    public void cambiarCodigo(String code, String newCode) {
+
+        if (maquina.getA1().getCodigo().equals(code)) {
+
+            maquina.getA1().setCodigo(newCode);
+
+        } else if (maquina.getA2().getCodigo().equals(code)) {
+
+            maquina.getA2().setCodigo(newCode);
+
+        } else if (maquina.getA3().getCodigo().equals(code)) {
+
+            maquina.getA3().setCodigo(newCode);
+
+        } else if (maquina.getA4().getCodigo().equals(code)) {
+
+            maquina.getA4().setCodigo(newCode);
+
+        } else if (maquina.getA5().getCodigo().equals(code)) {
+
+            maquina.getA5().setCodigo(newCode);
+
+        } else if (maquina.getA6().getCodigo().equals(code)) {
+
+            maquina.getA6().setCodigo(newCode);
+
+        } else if (maquina.getA7().getCodigo().equals(code)) {
+
+            maquina.getA7().setCodigo(newCode);
+
+        } else if (maquina.getA8().getCodigo().equals(code)) {
+
+            maquina.getA8().setCodigo(newCode);
+
+        } else {//Si no coincide con ninguno, mandamos un mensaje de error.
+
+            System.out.println("El código introducido no coincide con ningún"
+                    + " producto.");
+
+        }
+
+    }
+
+    public void cambiarProducto(String code, String newProducto) {
+
+        if (maquina.getA1().getCodigo().equals(code)) {
+
+            maquina.getA1().setNombre(newProducto);
+
+        } else if (maquina.getA2().getCodigo().equals(code)) {
+
+            maquina.getA2().setNombre(newProducto);
+
+        } else if (maquina.getA3().getCodigo().equals(code)) {
+
+            maquina.getA3().setNombre(newProducto);
+
+        } else if (maquina.getA4().getCodigo().equals(code)) {
+
+            maquina.getA4().setNombre(newProducto);
+
+        } else if (maquina.getA5().getCodigo().equals(code)) {
+
+            maquina.getA5().setNombre(newProducto);
+
+        } else if (maquina.getA6().getCodigo().equals(code)) {
+
+            maquina.getA6().setNombre(newProducto);
+
+        } else if (maquina.getA7().getCodigo().equals(code)) {
+
+            maquina.getA7().setNombre(newProducto);
+
+        } else if (maquina.getA8().getCodigo().equals(code)) {
+
+            maquina.getA8().setNombre(newProducto);
+
+        } else {//Si no coincide con ninguno, mandamos un mensaje de error.
+
+            System.out.println("El código introducido no coincide con ningún"
+                    + " producto.");
+
+        }
+
+    }
+
+    public void cambiarStock(String code, int nuevaCantidad) {
+
+        if (maquina.getA1().getCodigo().equals(code)) {
+
+            maquina.getA1().setCantidad(nuevaCantidad);
+
+        } else if (maquina.getA2().getCodigo().equals(code)) {
+
+            maquina.getA2().setCantidad(nuevaCantidad);
+
+        } else if (maquina.getA3().getCodigo().equals(code)) {
+
+            maquina.getA3().setCantidad(nuevaCantidad);
+
+        } else if (maquina.getA4().getCodigo().equals(code)) {
+
+            maquina.getA4().setCantidad(nuevaCantidad);
+
+        } else if (maquina.getA5().getCodigo().equals(code)) {
+
+            maquina.getA5().setCantidad(nuevaCantidad);
+
+        } else if (maquina.getA6().getCodigo().equals(code)) {
+
+            maquina.getA6().setCantidad(nuevaCantidad);
+
+        } else if (maquina.getA7().getCodigo().equals(code)) {
+
+            maquina.getA7().setCantidad(nuevaCantidad);
+
+        } else if (maquina.getA8().getCodigo().equals(code)) {
+
+            maquina.getA8().setCantidad(nuevaCantidad);
+
+        } else {//Si no coincide con ninguno, mandamos un mensaje de error.
+
+            System.out.println("El código introducido no coincide con ningún"
+                    + " producto.");
+
+        }
+
+    }
+
 }
