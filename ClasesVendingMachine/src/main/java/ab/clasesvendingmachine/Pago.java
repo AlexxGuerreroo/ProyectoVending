7a5efd5069930fd705239c7d4019109481f8c8b6
Alex Lopez Guerrero <alexx_guerreroo@outlook.es>
2022-02-09 19:29:38 +0100

    Cuando la máquina se queda sin stock de un artículo, la clase tarjeta ya no dejará introducir datos, aparecerá un mensaje de error directamente.

--- ClasesVendingMachine/src/main/java/ab/clasesvendingmachine/Pago.java
+++ ClasesVendingMachine/src/main/java/ab/clasesvendingmachine/Pago.java
@@ -12,7 +12,7 @@ import javax.swing.JOptionPane;
 
 public class Pago {
 
-    private boolean tarjetta;
+    private boolean conTarjeta;
     private Tarjeta tarjeta;
     private Deposito deposito;
     private double introducido;
@@ -23,10 +23,16 @@ public class Pago {
     private Deposito cambioM;
     private boolean fallo;
 
+    public Pago() {//Este constructor vacío se creará en situación de fallo.
+        
+        fallo = true;
+        
+    }    
+    
     //Contructor parametrizado para cuando se paga con tarjeta.
     public Pago(Tarjeta tarjeta, Deposito deposito, Bandeja producto) {
 
-        tarjetta = true;//La comprobación que determina si es pagado con tarjeta
+        conTarjeta = true;//La comprobación que determina si es pagado con tarjeta
         //o en metálico (en este caso es con tarjeta)
         this.tarjeta = tarjeta;//La tarjeta con la cual se pagará
         this.producto = producto;//El producto a pagar
@@ -37,6 +43,7 @@ public class Pago {
         horaPago = LocalTime.now();//Recoge la hora del pago
 
         this.tarjeta.setValido();//Comprueba si la tarjeta es válida
+        
         if (this.tarjeta.pago(producto.getPrecio())
                 && (this.producto.getCantidad() > 0)) {
             //Si la tarjeta puede realizar el pago y hay al menos un producto
@@ -50,13 +57,7 @@ public class Pago {
 
             fallo = false;//Y será realizado el pago con éxito
 
-        } else {//Si una de las dos condiciones falla, saltará error
-            if (this.producto.getCantidad() < 1) {
-                //Si no hay stock del producto a pagar...
-                JOptionPane.showMessageDialog(null, "El producto está agotado",
-                        "Fuera de Stock", JOptionPane.ERROR_MESSAGE);
-
-            }
+        } else {//Si una de las dos condiciones falla, saltará error            
 
             fallo = true;//Cancelará la operación
 
@@ -66,19 +67,20 @@ public class Pago {
 
     public Pago(Deposito deposito, Bandeja producto) {
 
-        tarjetta = false;//La comprobación que determina si es pagado con tarjeta
+        conTarjeta = false;//La comprobación que determina si es pagado con tarjeta
         //o en metálico (en este caso es en metálico)
         introducido = 0;
-        this.producto = producto;//El producto a pagar
-        this.deposito = deposito;//El depósito de dinero de la máquina
-        double aux;//Una variable auxiliar que ayudará respecto a realizar cambio
-        cambioM = new Deposito();//Un "depósito" que devolverá el cambio en monedas
+        int precio = (int) (producto.getPrecio() * 10);
+        this.producto = producto;//El producto a pagar.
+        this.deposito = deposito;//El depósito de dinero de la máquina.
+        double aux;//Una variable auxiliar que ayudará respecto a realizar cambio.
+        cambioM = new Deposito();//Un "depósito" que devolverá el cambio en monedas.
         cambioM.vaciarDeposito();//Vaciamos el depósito del cambio.
         Deposito insertar = new Deposito();//Un "Depósito" auxiliar que añadirá 
         //al depósito de la máquina la cantidad introducida de monedas y billetes.
-        insertar.vaciarDeposito();//Vaciamos el depósito donde que se insertará el dinero
-        fechaPago = LocalDate.now();//Recoge la fecha del pago
-        horaPago = LocalTime.now();//Recoge la hora del pago
+        insertar.vaciarDeposito();//Vaciamos el depósito donde que se insertará el dinero.
+        fechaPago = LocalDate.now();//Recoge la fecha del pago.
+        horaPago = LocalTime.now();//Recoge la hora del pago.
 
         if (this.producto.getCantidad() > 0) {//Si el producto no está agotado
 
@@ -86,9 +88,9 @@ public class Pago {
              * ******************Para introducir el pago**********************
              */
             
-            //Realizamos un bucle para pedir qué monedar introducir hasta alcanzar
-            //el precio del producto:
-            while (introducido < producto.getPrecio()) {
+            //Realizamos un bucle para pedir qué monedar se va a introducir.
+            //Se efectuará el bucle hasta que se finalice el pago.
+            while (introducido < precio) {
                     
                 //Array que almacena las opciones de monedas que se van a poder introducir.
                 String[] options = {"10 céntimos", "20 céntimos", "50 céntimos",
@@ -96,56 +98,56 @@ public class Pago {
                 
                 //Pedimos en una ventana el tipo de moneda que se va a introducir
                 //y lo recoge en un String llamado entry
-                String entry = (String) JOptionPane.showInputDialog(null, "¿Qué desa moneda introducir?"
-                        + "\n(Queda por pagar: " + (producto.getPrecio() - introducido)
+                String entry = (String) JOptionPane.showInputDialog(null, "¿Qué moneda desea introducir?"
+                        + "\n(Queda por pagar: " + ((double)(precio - introducido) / 10)
                         + "0€)", "Pasarela de Pago", JOptionPane.QUESTION_MESSAGE,
                          null, options, options[0]);                                
 
                 switch (entry) {
-                    //Según el valor de entry, se añade a un depósito
-                    //la moneda o billete introducido
+                    //Según el valor de entry, se añade a un depósito la moneda/billete
+                    //introducido.
 
                     case "10 céntimos":
                         insertar.addM10c(1);
-                        introducido += 0.1;
+                        introducido += 1;
                         break;
 
                     case "20 céntimos":
                         insertar.addM20c(1);
-                        introducido += 0.2;
+                        introducido += 2;
                         break;
 
                     case "50 céntimos":
                         insertar.addM50c(1);
-                        introducido += 0.5;
+                        introducido += 5;
                         break;
 
                     case "1€":
                         insertar.addM1e(1);
-                        introducido += 1;
+                        introducido += 10;
                         break;
 
                     case "2€":
                         insertar.addM2e(1);
-                        introducido += 2;
+                        introducido += 20;
                         break;
 
                     case "5€":
                         insertar.addB5e(1);
-                        introducido += 5;
+                        introducido += 50;
                         break;
 
                     case "10€":
                         insertar.addB10e(1);
-                        introducido += 10;
+                        introducido += 100;
                         break;
 
                     case "20€":
                         insertar.addB20e(1);
-                        introducido += 20;
+                        introducido += 200;
                         break;
                         
-                    case "null"://Si se da a volver, se anula el pago
+                    case "null"://Si se da a volver, se anula el pago.
                         fallo = true;
                         break;
 
@@ -160,34 +162,33 @@ public class Pago {
                     insertar.getM50c(), insertar.getM1e(), insertar.getM2e(),
                     insertar.getB5e(), insertar.getB10e(), insertar.getB20e());
 
-            /**
+            /*
              * ********************Para aportar el cambio*********************
              */
-            cambio = introducido - producto.getPrecio();//Se calcula el cambio
+            
+            cambio = introducido - precio;//Se calcula el cambio
 
-            aux = cambio * 100;//Se multiplica por 100 (se verá su utilidad más adelante)
+            aux = cambio;//Se multiplica por 100 (se verá su utilidad más adelante)
 
             //Algoritmo para decidir cuantas monedas se dan en el cambio:
-            cambioM.addM2e((int) aux / 200);//Se divide por 200 para obtener 
+            cambioM.addM2e((int) aux / 20);//Se divide por 200 para obtener 
             //las monedas de 2€ y se resta al auxiliar           
-            aux -= cambioM.getM2e() * 200;
+            aux -= cambioM.getM2e() * 20;
 
-            cambioM.addM1e((int) aux / 100);//Se divide por 100 para obtener 
+            cambioM.addM1e((int) aux / 10);//Se divide por 100 para obtener 
             //las monedas de 1€ y se resta al auxiliar           
-            aux -= cambioM.getM1e() * 100;
+            aux -= cambioM.getM1e() * 10;
 
-            cambioM.addM50c((int) aux / 50);//Se divide por 50 para obtener 
+            cambioM.addM50c((int) aux / 5);//Se divide por 50 para obtener 
             //las monedas de 50 céntimos y se resta al auxiliar          
-            aux -= cambioM.getM50c() * 50;
+            aux -= cambioM.getM50c() * 5;
 
-            cambioM.addM20c((int) aux / 20);//Se divide por 20 para obtener 
+            cambioM.addM20c((int) aux / 2);//Se divide por 20 para obtener 
             //las monedas de 20 céntimos y se resta al auxiliar          
-            aux -= cambioM.getM20c() * 20;
+            aux -= cambioM.getM20c() * 2;
 
-            cambioM.addM10c((int) aux / 10);//Se divide por 10 para obtener 
+            cambioM.addM10c((int) aux);//Se divide por 10 para obtener 
             //las monedas de 10 céntimos
-            aux -= cambioM.getM10c() * 10;
-            cambio = aux / 100;
 
             if (this.deposito.coinCheck(cambioM)) {//Se comprueba si el depósito tiene
                 //la cantidad de monedas necesarias para el cambio.
@@ -204,8 +205,8 @@ public class Pago {
             } else {  //En el caso de que no haya cambio suficiente nos aparecerá
                 //un error que nos impida seguir con el pago del artículo.
 
-                JOptionPane.showMessageDialog(null, "Lo sentimos la máquina no "
-                        + "dispone de cambio sufieciente, deberá pagar la cantidad justa",
+                JOptionPane.showMessageDialog(null, "Lo sentimos, la máquina no "
+                        + "dispone de cambbio suficiente.\t Tendrá que pagar la cantidad justa",
                         "Pago Insuficiente", JOptionPane.ERROR_MESSAGE);
 
                 //Y quitamos todo lo que hemos añadido en la operación:
@@ -219,44 +220,35 @@ public class Pago {
 
             }
 
-        } else {//Si no hay stock del producto solicitado...
-
-            JOptionPane.showMessageDialog(null, "El producto está agotado",
-                    "Fuera de Stock", JOptionPane.ERROR_MESSAGE);
-
-            fallo = true;
-
         }
 
     }
 
-    @Override
-    public String toString() {//Se muestra como si se tratase de un recibo
+    @Override   //toString que muestra los racibos al efectuar el pago.
+    public String toString() {
 
-        if (fallo) {//Si el pago falla, será mostrado este mensaje:
+        if (fallo) {//Si el pago falla, se mostrará este mensaje.
 
             return "OPERACIÓN CANCELADA";
 
-        } else if (tarjetta) {//Si se ha pagado con tarjeta, se muestra este 
-            //tipo de recibo:
+        } else if (conTarjeta) {//Si se paga con tarjeta, se muestra este recibo.
 
             return "Factura{\nPagado con tarjeta "
                     + "\nProducto comprado: " + producto.forFactura()
                     + "\nDinero introducido: " + introducido
                     + "€\nFecha:  " + fechaPago + "\n}";
 
-        } else {//Si se ha pagado en metálico, se muestra este 
-            //tipo de recibo:
+        } else {//Si se paga en metálico, se muestra este recibo. 
+
             DateTimeFormatter formatter = DateTimeFormatter.ofPattern("HH:mm:ss");
             
             return "Factura{ \nPagado en metálico "
                     + "\nProducto comprado: " + producto.forFactura()
-                    + "\nDinero introducido: " + introducido
+                    + "\nDinero introducido: " + (introducido/10)
                     + "€\nCambio : \n" + '(' + cambioM.mostrarCambio() + "\n"
                     + ')' + "\nFecha: " + fechaPago + "\nHora: "
                     + horaPago.format(formatter) + "\n}";
 
         }
     }
-
 }
